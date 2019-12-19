3496a7f3e367d0d2ff288602905714f72c5b2358
tejasrinivasraj <1tejasrinivasraj@gmail.com>
2019-12-19 16:33:20 +0530

    [Teja] Practice03 Comparision of Byte and Int

---
+++ P02.java
@@ -0,0 +1,13 @@
+// Obj referece var overwritting
+// Will result in error.
+
+import java.util.*;
+public class P02{
+    public static void main(String args[]){
+        Object s = new Object();
+
+        Scanner s1 = new Scanner(System.in);
+        s = s2;
+        String s2 = new String();
+    }
+}
---
+++ P03.java
@@ -0,0 +1,14 @@
+// Byte and Int comparision.
+
+public class P03{
+    public static void main(String args[]){
+        int n = 3;
+        byte m = 3;
+        if(n==m){
+            System.out.println("Hello");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
