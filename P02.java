279cc1ac9b96ed6a2e67ec02ea9f39522ed2c608
tejasrinivasraj <1tejasrinivasraj@gmail.com>
2019-12-19 16:37:58 +0530

    [Teja] Practice02 Error Obj reference var overwriting.

--- P02.java
+++ P02.java
@@ -8,7 +8,7 @@ public class P02{
 
         Scanner s1 = new Scanner(System.in);
         s = s2;
-        System.out.println("Error!");
+        System.out.println("Error after this!");
         String s2 = new String();
     }
 }
